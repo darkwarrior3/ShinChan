6297c86083a62383695da4b3c76d2e4556f2f176
Dark Warrior <anonymous000333@protonmail.com>
2019-05-21 00:09:40 +0530

    Add files via upload

---
+++ shinchaner.sh
@@ -0,0 +1,10 @@
+#!/data/data/com.termux/files/usr/bin/bash
+apt update && apt upgrade
+apt install w3m figlet
+clear
+mv shinchan.sh /$HOME/../usr/bin/shinchan
+chmod +x /$HOME/../usr/bin/shinchan
+cd ..
+rm -rf ShinChan
+figlet ShinChan
+echo To start ShinChan just type shinchan in your console
